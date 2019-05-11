c46c5a634cd9436cbb7a9386a243e27cef7fd541
tanderson <treveranderson01@gmail.com>
2019-05-11 17:56:25 -0500

    got db up and database and table created on startup

---
+++ database/db_create.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE test;
+USE test;
+CREATE TABLE deep_learning ( id smallint unsigned not null auto_increment, name varchar(20) not null, constraint pk_example primary key (id) );
--- db_create.sql
+++
@@ -1,3 +0,0 @@
-CREATE DATABASE dbname;
-USE dbname;
-CREATE TABLE tablename ( id smallint unsigned not null auto_increment, name varchar(20) not null, constraint pk_example primary key (id) );
