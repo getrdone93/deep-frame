d78e588d8ac22a4b96fd3d1b0edf900056a8e29f
tanderson <treveranderson01@gmail.com>
2019-05-22 19:46:15 -0500

    got correct args, config, and data, but nasty cudnn loading error needs to be fixed

--- tensorflow_test_model/test_container.py
+++ tensorflow_test_model/test_container.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 from absl import flags
+import time
 
 flags.DEFINE_string(
     'model_dir', None, 'Path to output model directory '
@@ -9,14 +10,21 @@ flags.DEFINE_string('pipeline_config_path', None, 'Path to pipeline config '
 FLAGS = flags.FLAGS
 
 def main(unused_argv):
-    flags.mark_flag_as_required('model_dir')
-    flags.mark_flag_as_required('pipeline_config_path')
+    while True:
+        print("Training and stuff")
+        time.sleep(1)
 
-    print("I made it here, flags ", FLAGS.model_dir, FLAGS.pipeline_config_path)
+    # flags.mark_flag_as_required('model_dir')
+    # flags.mark_flag_as_required('pipeline_config_path')
 
-    f = open("/data/test_file.txt", "a")
-    f.write("This is some dater")
-    f.close()
+    # print("I made it here, flags ", FLAGS.model_dir, FLAGS.pipeline_config_path)
+
+    # f = open("/data/test_file.txt", "a")
+    # f.write("This is some dater")
+    # f.close()
+
+    # f = open("/data/test_file.txt", "r")
+    # print(f.read())
 
 if __name__ == '__main__':
     tf.app.run()
