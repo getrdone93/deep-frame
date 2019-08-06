d3b1f922367d7883e7c6ad6b7423bf6b0c6fe02c
Trever Anderson <treveranderson01@gmail.com>
2019-08-05 20:34:32 -0500

    can associate file names to files for multiple files

--- data_split.py
+++ data_split.py
@@ -1,6 +1,12 @@
 import json
 import argparse
 
+# def dep_func(**kwargs):
+#     perc, tot = percentages(category_counts=ncs)
+#     val = sort_by_val(data_map=perc)
+#     print_data(data_map=val)
+#     print("Total: %d" % (tot))
+
 def read_file(**kwargs):
     data_file = kwargs['data_file']
 
@@ -57,33 +63,45 @@ def map_from_coll(**kwargs):
 
     return {e[k]: {ek: e[ek] for ek in set(e.keys()).difference({k})} for e in c}
 
+def images_for_file(**kwargs):
+    inst_m, cat_key, ak, im_id, i_d, im = kwargs['instance_map'], kwargs['categories'], kwargs['annotations'],\
+                                          kwargs['image_id'], kwargs['id'], kwargs['image_map']
+
+    mappings = {}
+    for insf, t_dir in inst_m.iteritems():    
+        data = read_file(data_file=insf)
+        data_maps = {kc[0]: map_from_coll(collection=kc[1], key=i_d) for kc in 
+         ((cat_key, data[cat_key]), (images, data[images]), (ak, data[ak]))}
+
+        imgs_bc = images_by_category(data=data, anno_key=ak, category_id=cat_id, 
+                                     image_id=im_id, i_d=i_d, image_map_key=im, images=data_maps[images])
+        ncs = name_categories(id_key=i_d, name_key=nk, category_data=data_maps[cat_key], 
+                        images_by_category=imgs_bc)
+        fns = category_file_names(categories=args.category_names, images_by_category=ncs, 
+                                  file_name_key=fn, image_map_key=im)
+        mappings[t_dir] = fns
+    
+    return mappings    
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Split a dataset')
-    parser.add_argument('--instance-files', nargs='+', required=True, help='Annotations file')
+    parser.add_argument('--instance-files', nargs='+', required=True, help='Annotations files')
     parser.add_argument('--category-names', nargs='+', required=True, help='Categories')
     parser.add_argument('--percentages', required=False, action='store_true')
+    parser.add_argument('--directories', nargs='+', required=True, help='Paths of data dirs. Should correspond ' 
+                        + 'one-to-one with instance-files')
     args = parser.parse_args()
+    if len(args.instance_files) != len(args.directories):
+        print('instance-files and directories must have same len as they map one-to-one')
+        exit(0)
+    instances_dirs = {v: args.directories[i] for i, v in enumerate(args.instance_files)}
     cat_key, ak, cat_id, im_id, i_d, nk, images, im, fn = 'categories', 'annotations', 'category_id',\
                                                   'image_id', 'id', 'name', 'images', 'image_map',\
                                                   'file_name'
-    data = read_file(data_file=args.instance_files[0])
-    print('data keys: ' + str(data.keys()))
-
-    data_maps = {kc[0]: map_from_coll(collection=kc[1], key=i_d) for kc in 
-     ((cat_key, data[cat_key]), (images, data[images]), (ak, data[ak]))}
-    
-    imgs_bc = images_by_category(data=data, anno_key=ak, category_id=cat_id, 
-                                 image_id=im_id, i_d=i_d, image_map_key=im, images=data_maps[images])
-    ncs = name_categories(id_key=i_d, name_key=nk, category_data=data_maps[cat_key], 
-                    images_by_category=imgs_bc)
-    if args.percentages:
-        perc, tot = percentages(category_counts=ncs)
-        val = sort_by_val(data_map=perc)
-        print_data(data_map=val)
-        print("Total: %d" % (tot))
-    else:
-        fns = category_file_names(categories=args.category_names, images_by_category=ncs, 
-                                  file_name_key=fn, image_map_key=im)
-        for k, v in fns.iteritems():
+    key_args = {cat_key: cat_key, ak: ak, im_id: im_id, i_d: i_d, im: im}
+    ms = images_for_file(instance_map=instances_dirs, **key_args)
+    for f, fv in ms.iteritems():
+        print('file ' + f)
+        for k, v in fv.iteritems():
             print((k, len(v), v))
     
