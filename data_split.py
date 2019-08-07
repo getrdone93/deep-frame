35c23245d5bd6e67e4fc907c8b535515c246b3ce
Trever Anderson <treveranderson01@gmail.com>
2019-08-06 19:11:45 -0500

    count data split by image id

--- data_split.py
+++ data_split.py
@@ -3,12 +3,7 @@ import argparse
 import os.path as path
 import shutil
 import os
-
-# def dep_func(**kwargs):
-#     perc, tot = percentages(category_counts=ncs)
-#     val = sort_by_val(data_map=perc)
-#     print_data(data_map=val)
-#     print("Total: %d" % (tot))
+import glob
 
 def read_file(**kwargs):
     data_file = kwargs['data_file']
@@ -19,16 +14,15 @@ def read_file(**kwargs):
     return data
 
 def images_by_category(**kwargs):
-    d, ak, cid, img_id, i_d, img_mk, imgs = kwargs['data'], kwargs['anno_key'],\
+    d, ak, cid, img_id, i_d, imgs = kwargs['data'], kwargs['anno_key'],\
                                       kwargs['category_id'], kwargs['image_id'],\
-                                      kwargs['i_d'], kwargs['image_map_key'], kwargs['images']
+                                      kwargs['i_d'], kwargs['images']
 
     cat_img = {}
     for e in d[ak]:
         if e[cid] not in cat_img:
             cat_img[e[cid]] = []
-        cat_img[e[cid]].append({i_d: e[i_d], img_id: e[img_id], 
-                                img_mk: imgs[e[img_id]]})
+        cat_img[e[cid]].append({i_d: e[i_d], img_id: e[img_id]})
         
     return cat_img
 
@@ -43,33 +37,39 @@ def name_categories(**kwargs):
 
     return {cats[k][nk]: im_bc[k] for k in im_bc.keys()}
 
-def print_data(**kwargs):
-    d = kwargs['data_map']
-
-    for e in d:
-        print("key: %s\t(%d, %f)" % (e[0], e[1][1], e[1][2]))
-
 def category_file_names(**kwargs):
-    cs, imgs_bc, fnk, imk = kwargs['categories'], kwargs['images_by_category'], kwargs['file_name_key'],\
-                            kwargs['image_map_key']
+    cs, imgs_bc, fnk, im_id, imgs = kwargs['categories'], kwargs['images_by_category'], kwargs['file_name_key'],\
+                                    kwargs['image_id'], kwargs['all_images']
 
-    return {k: map(lambda e: e[imk][fnk], imgs_bc[k]) for k in cs}
+    return {k: map(lambda e: imgs[e[im_id]][fnk], imgs_bc[k]) for k in cs}
 
 def percentages(**kwargs):
-    cs = kwargs['category_counts']
+    cs, ck = kwargs['category_counts'], kwargs['count_key']
 
-    total = float(sum(map(lambda v: len(v), cs.values())))
-    return {k: (cs[k], len(cs[k]), round(len(cs[k]) / total, 5)) for k in cs.keys()}, total
+    total = float(len(reduce(lambda s1, s2: s1.union(s2), map(lambda l: set(map(lambda m: m[ck], l)), cs.values()))))
+    return {k: (cs[k], len(set(map(lambda m: m[ck], cs[k]))), 
+                round(len(set(map(lambda m: m[ck], cs[k]))) / total, 5)) for k in cs.keys()}, total
 
 def map_from_coll(**kwargs):
     c, k = kwargs['collection'], kwargs['key']
 
     return {e[k]: {ek: e[ek] for ek in set(e.keys()).difference({k})} for e in c}
 
-def images_for_file(**kwargs):
-    inst_m, cat_key, ak, im_id, i_d, im = kwargs['instance_map'], kwargs['categories'], kwargs['annotations'],\
-                                          kwargs['image_id'], kwargs['id'], kwargs['image_map']
+def output_percentages(**kwargs):
+    ncs, fn, dm, ims, im_id = kwargs['name_categories'], kwargs['file_name'], kwargs['data_maps'],\
+                          'images', kwargs['image_id']
 
+    perc, tot = percentages(category_counts=ncs, count_key=im_id)
+    val = sort_by_val(data_map=perc)
+    print('File: ' + str(fn))
+    for e in val:
+        print("key: %s\t(%d, %f)" % (e[0], e[1][1], e[1][2]))
+    print("Total: %d\n" % (tot))
+
+def images_for_file(**kwargs):
+    inst_m, cat_key, ak, im_id, i_d, cat_id, images, fn = kwargs['instance_map'], kwargs['categories'], kwargs['annotations'],\
+                                          kwargs['image_id'], kwargs['id'], 'category_id', 'images',\
+                                          'file_name'
     mappings = {}
     for insf, ds in inst_m.iteritems():    
         data = read_file(data_file=insf)
@@ -77,11 +77,12 @@ def images_for_file(**kwargs):
          ((cat_key, data[cat_key]), (images, data[images]), (ak, data[ak]))}
 
         imgs_bc = images_by_category(data=data, anno_key=ak, category_id=cat_id, 
-                                     image_id=im_id, i_d=i_d, image_map_key=im, images=data_maps[images])
+                                     image_id=im_id, i_d=i_d, images=data_maps[images])
         ncs = name_categories(id_key=i_d, name_key=nk, category_data=data_maps[cat_key], 
                         images_by_category=imgs_bc)
+        output_percentages(name_categories=ncs, file_name=path.basename(insf), data_maps=data_maps, image_id=im_id)
         fns = category_file_names(categories=args.category_names, images_by_category=ncs, 
-                                  file_name_key=fn, image_map_key=im)
+                                  file_name_key=fn, image_id=im_id, all_images=data_maps[images])
         mappings[ds] = fns
     
     return mappings
@@ -95,6 +96,7 @@ def write_files(**kwargs):
             fns = map(lambda f: (path.join(ft[0], f), path.join(ft[1], f)), fs)
             for f in fns:
                 shutil.copyfile(f[0], f[1])
+        print("Files in %s: %d" % (ft[1], len(glob.glob(path.join(ft[1], './*')))))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Split a dataset')
@@ -114,10 +116,8 @@ if __name__ == '__main__':
         if not path.exists(d):
             os.makedirs(d)
     instances_dirs = {v: (args.current_dirs[i], args.target_dirs[i]) for i, v in enumerate(args.instance_files)}
-    cat_key, ak, cat_id, im_id, i_d, nk, images, im, fn = 'categories', 'annotations', 'category_id',\
-                                                  'image_id', 'id', 'name', 'images', 'image_map',\
-                                                  'file_name'
-    key_args = {cat_key: cat_key, ak: ak, im_id: im_id, i_d: i_d, im: im}
+    cat_key, ak, im_id, i_d, nk = 'categories', 'annotations', 'image_id', 'id', 'name'
+    key_args = {cat_key: cat_key, ak: ak, im_id: im_id, i_d: i_d}
     ms = images_for_file(instance_map=instances_dirs, **key_args)
     write_files(mappings=ms)
     
