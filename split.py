352489bda4df7c704727f37f01829e49f5838b6e
Trever Anderson <treveranderson01@gmail.com>
2019-09-03 19:50:23 -0500

    can copy from val, split, and into val and test

--- split.py
+++ split.py
@@ -1,5 +1,8 @@
 import argparse
 import json
+import os.path as path
+import shutil
+import os
 
 CAT_KEY = 'categories'
 ANNOTATIONS = 'annotations'
@@ -7,6 +10,7 @@ IMAGE_ID = 'image_id'
 I_D = 'id'
 CAT_ID_KEY = 'category_id'
 IMAGES = 'images'   
+FILE_NAME = 'file_name'
 
 def read_file(path):
     with open(path, 'r') as df:
@@ -57,14 +61,25 @@ def filter_data(data, image_ids, anno_ids, images_key, anno_key, id_key):
         new_data[k] = data[k]
     return new_data
 
+def copy_images(images, src, dest, file_name_key):
+    for im_id, img in images.iteritems():
+        ft = map(lambda f: path.join(path.abspath(f), 
+                                img[file_name_key]), (src, dest))
+        shutil.copyfile(ft[0], ft[1])
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Split a dataset')
     parser.add_argument('--instance-file', required=False)
     parser.add_argument('--image-dir', required=False)
     parser.add_argument('--validation-ids-path', required=False)
-    parser.add_argument('--target-train', nargs=1, required=False)
-    parser.add_argument('--target-validation', nargs=1, required=False)
+    parser.add_argument('--target-train', nargs=2, required=False)
+    parser.add_argument('--target-validation', nargs=2, required=False)
     args = parser.parse_args()
+
+    # make_dirs = map(lambda d: os.makedirs(d[1]), 
+    #     filter(lambda e: not e[0], map(lambda f: (path.exists(f), f), 
+    #                            (args.target_train[0], args.target_validation[0]))))
+    # print('Made these dirs: ' + str(make_dirs))
 
     ids = ids_from_path(path=args.validation_ids_path, read_func=read_file)
     data, data_maps = annotations_from_disk(args.instance_file, read_func=read_json_file, 
@@ -73,9 +88,20 @@ if __name__ == '__main__':
 
     train, val = split_data(data_maps=data_maps, val_ids=ids, images_key=IMAGES, anno_key=ANNOTATIONS, 
                image_id_key=IMAGE_ID)
-    filt_data = filter_data(data=data, image_ids=set(val[0]), anno_ids=set(val[1]), 
+    filt_val_data = filter_data(data=data, image_ids=set(val[0]), anno_ids=set(val[1]), 
+                            images_key=IMAGES, anno_key=ANNOTATIONS, id_key=I_D)
+    filt_train_data = filter_data(data=data, image_ids=set(train[0]), anno_ids=set(train[1]), 
                             images_key=IMAGES, anno_key=ANNOTATIONS, id_key=I_D)
 
-    write_json_file(path=args.target_validation[0], data=filt_data)
+
+    print('copying validation')
+    copy_images(images=val[0], src=args.image_dir, dest=args.target_validation[0], 
+                file_name_key=FILE_NAME)
+    write_json_file(path=args.target_validation[1], data=filt_val_data)
+
+    print('copying training')
+    copy_images(images=train[0], src=args.image_dir, dest=args.target_train[0], 
+                file_name_key=FILE_NAME)
+    write_json_file(path=args.target_train[1], data=filt_train_data)
 
 
